701d7aa09eb222f11d924d8650aa13eaddeebdb3
Jim Lind <spoon.vw@gmail.com>
2011-01-24 19:58:02 -0600

    Next and Previous Navigation Created.

--- display.php
+++ display.php
@@ -3,24 +3,29 @@
  * THIS HEADER GIVES YOU PROPER SYMBOL OUTPUT
  */
 
-echo "<html xmlns='http://www.w3.org/1999/xhtml' xml:lang='en' lang='en'><head><base href='http://www.wiscnews.com/content/tncms/live/'>";
+echo "<html xmlns='http://www.w3.org/1999/xhtml' xml:lang='en' lang='en'><head>";
 echo "<meta http-equiv='Content-Type' content='text/html; charset=utf-8'></head>";
 echo "<body>";
 
 $files = array();
 $dir = opendir('./xml/');
-//$files = readdir($dir);
+
+$fileNumber = 0;
 
 while(($file = readdir($dir)) !== false) {
 	if($file !== '.' && $file !== '..' && !is_dir($file) && $file{0} === "b") {
 		$files[] = $file;
 	}
 }
-
 rsort($files);
 
-$file = fopen("./xml/".$files[0], "r");
-$fileContents = fread($file, filesize("./xml/".$files[0]));
+$timestamp = intval(filter_input(INPUT_GET, "b", FILTER_SANITIZE_NUMBER_INT));
+$index = array_search("b$timestamp.xml", $files);
+if ($index !== false) $fileNumber = $index;
+
+$file = fopen("./xml/".$files[$fileNumber], "r");
+$fileContents = fread($file, filesize("./xml/".$files[$fileNumber]));
+
 fclose($file);
 //Replace the whitespace with nothing
 $fileContents = preg_replace("/>\s+</", "><", $fileContents);
@@ -35,4 +40,12 @@ foreach ($xml->childNodes AS $p) {
 	echo "</p>";
 }
 
+echo "<ul>";
+if (isset($files[$fileNumber-1])) {
+	echo "<li><a href='display.php?b=".$files[$fileNumber-1]."'>Previous</a></li>";
+}
+if (isset($files[$fileNumber+1])) {
+	echo "<li><a href='display.php?b=".$files[$fileNumber+1]."'>Next</a></li>";
+}
+echo "<ul>";
 echo "</body>";
