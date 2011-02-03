b1aa4729c48c3e6847eb35dfd986a1e0eaedb0ae
Jim Lind <spoon.vw@gmail.com>
2011-02-02 21:57:35 -0600

    JavaScript and CSS for the #number tags magic to work as planned.

--- js/web.js
+++ js/web.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
 	$(window).scroll(function(){
-		$top = $(this).scrollTop();
+		var $top = $(this).scrollTop();
 		$("#previous").css('top', ($top)+'px');
 		$("#next").css('top', ($top)+'px');
 	});
 	
 	$("#previous, #next").bind('click', function(e) {
 		e.preventDefault();
-		$link = $(e.target).attr('href');
+		var $link = $(e.target).attr('href');
 		window.location = $link;
 	});
 	
@@ -16,4 +16,15 @@ $(document).ready(function() {
 	    wipeRight: function() { $("#previous").trigger('click') },
 	    preventDefaultEvents: false
 	});
+	
+	var $url = window.location.toString();
+	var $index = parseInt($url.substr($url.lastIndexOf("#")+1));
+	if ($index >= 0) {
+		var $head = $("#main h3:eq("+$index+")");
+		var $offset = $($head).offset().top - 35;
+		$('html,body').animate({scrollTop: $offset});
+		
+		var $content = $head.add($($head).nextUntil('h3'));		
+		$content.wrapAll('<div class="highlight" />');
+	}
 });
