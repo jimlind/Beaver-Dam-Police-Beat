37fe450bfe85d753b9e33a3f81f8f8e8d42c67a5
Jim Lind <spoon.vw@gmail.com>
2011-02-09 19:38:38 -0600

    Removed excessive Echo

--- index.php
+++ index.php
@@ -24,23 +24,21 @@
 	
 	$time = intval(substr($files[$fileNum], 1));
 	$link = "index.php?b=$time";
-
-	echo "<html xmlns='http://www.w3.org/1999/xhtml' xml:lang='en' lang='en'>";
-	echo "<head>";
-	echo "<title>Beaver Dam Police Beat for ".date("M jS Y - g:i a", $time)."</title>";
-	echo "<meta http-equiv='Content-Type' content='text/html; charset=utf-8'>";
-	echo "<link href='http://fonts.googleapis.com/css?family=Droid+Sans' rel='stylesheet' type='text/css'>";
-	echo "<link rel='stylesheet' type='text/css' href='css/web.css'>";
-	echo "<link rel='stylesheet' media='only screen and (max-device-width: 1024px) and (orientation:portrait)' href='css/ipad.portrait.css' type='text/css' />";
-	echo "<link rel='stylesheet' media='only screen and (max-device-width: 1024px) and (orientation:landscape)' href='css/ipad.landscape.css' type='text/css' />";
-	echo "<script src='js/jquery-1.4.4.min.js' type='text/javascript'></script>";
-	echo "<script src='js/jquery.touchwipe.min.js' type='text/javascript'></script>";
-	echo "<script src='js/web.js' type='text/javascript'></script>";
-	
-$googleAnalytics = <<<GAJSCRIPT
+?>
+<html xmlns='http://www.w3.org/1999/xhtml' xml:lang='en' lang='en'>
+<head>
+<title>Beaver Dam Police Beat for <?= date("M jS Y - g:i a", $time) ?></title>
+<meta http-equiv='Content-Type' content='text/html; charset=utf-8'>
+<link href='http://fonts.googleapis.com/css?family=Droid+Sans' rel='stylesheet' type='text/css'>
+<link rel='stylesheet' type='text/css' href='css/web.css'>
+<link rel='stylesheet' media='only screen and (max-device-width: 1024px) and (orientation:portrait)' href='css/ipad.portrait.css' type='text/css' />
+<link rel='stylesheet' media='only screen and (max-device-width: 1024px) and (orientation:landscape)' href='css/ipad.landscape.css' type='text/css' />
+<script src='js/jquery-1.4.4.min.js' type='text/javascript'></script>
+<script src='js/jquery.touchwipe.min.js' type='text/javascript'></script>
+<script src='js/web.js' type='text/javascript'></script>
 <script type="text/javascript">
 var _gaq = _gaq || [];
-_gaq.push(['_setAccount', '$goog_analytics_key']);
+_gaq.push(['_setAccount', '<?= $goog_analytics_key ?>']);
 _gaq.push(['_trackPageview']);
 (function() {
 var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
@@ -48,13 +46,12 @@ ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www')
 var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 })();
 </script>
-GAJSCRIPT;
+</head>
+<body>
+
+<h1>Beaver Dam Police Beat</h1>
 	
-	echo $googleAnalytics;	
-	echo "</head>";
-	echo "<body>";
-	
-	echo "<h1>Beaver Dam Police Beat</h1>";
+<?php
 	echo "<div id='main'>";
 	if ($prev) {
 		echo "<a id='previous' href='$prev'><span>Previous</span></a>";
@@ -96,5 +93,6 @@ GAJSCRIPT;
 	if ($next) {
 		echo "<li><a href='$next'>Next</a></li>";
 	}
-	echo "<ul>";	
-	echo "</body>";
+	echo "<ul>";
+?>
+</body>
